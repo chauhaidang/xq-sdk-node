bdc5a2e9408614f3bee4af88dc79456d3f712881
chauhaidang1 <chauhaidang1@gmail.com>
2024-06-30 22:46:25 +0700

    add stub-server and pump minor to 0.1.0

--- blackbox_test/grpcclient.spec.ts
+++ blackbox_test/grpcclient.spec.ts
@@ -1,11 +1,11 @@
 import { GrpcClient } from "../src/client/grpc_client";
-const protoPath = `${__dirname}/stub-server/grpc/protos/blog.proto`;
+const protoPath = `${__dirname}/../stub-server/grpc/protos/blog.proto`;
 const baseUrl = "localhost:4312";
 
 describe("GrpcClient", () => {
   it("should return valid response", async () => {
     const metadata = {};
-    const payload = { id: 1, title: "abc" };
+    const payload = { id: 1, title: "something" };
     const client = new GrpcClient(
       protoPath,
       "blogPackage",
@@ -14,6 +14,8 @@ describe("GrpcClient", () => {
       metadata,
     );
     const res = await client.createBlogSync(payload);
-    expect(res).toEqual(payload);
+    expect(res.title).toEqual(payload.title);
+    expect(res.id).toBeGreaterThanOrEqual(500);
+    expect(res.id).toBeLessThanOrEqual(600);
   });
 });
